81bcfccc2764b84ba1f5781fb55a2dd51344c3be
sangbyuk <jsb100800@naver.com>
2022-01-03 15:26:27 +0900

    전화번호 목록

--- 해시/전화번호 목록.py
+++ 해시/전화번호 목록.py
@@ -4,13 +4,17 @@ def solution(phone_book):
 
     answer = True
 
+    # 공백 제거
     for p in phone_book:
         p = p.replace(" ","")
 
+    # 사전순 정렬
     phone_book.sort()
-    print(phone_book)
-    # for p in phone_book:
 
+    # 반복문을 돌면서 접두인지 검사.
+    for i in range(0,len(phone_book) - 1):
+        if phone_book[i+1].startswith(phone_book[i]):
+            return False
 
     return answer
 
