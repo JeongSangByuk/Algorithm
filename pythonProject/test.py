57d3f5167e84882f3b955b4588a7d4176759c44f
JeongSangByuk <jsb100800@naver.com>
2022-02-13 15:40:51 +0900

    순위

--- pythonProject/test.py
+++ pythonProject/test.py
@@ -1,18 +1,4 @@
-n = int(input())
-m = int(input())
-x = int(input())
-count = 0
-#a = list(range(1, n * m + 1, m))
-a = []
 
-for i in range(1, n * m + 1, m):
-    a.append(i)
-    if i < x:
-        count+=1
+a = set()
 
 print(a)
-
-if count == 0:
-    print("None")
-else:
-    print("count =",count)
