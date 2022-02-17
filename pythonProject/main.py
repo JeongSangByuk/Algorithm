dff7daf95f48dc791e03796ddb928a93bf30ecf3
JeongSangByuk <jsb100800@naver.com>
2022-02-17 18:40:57 +0900

    신입사원

--- pythonProject/main.py
+++ pythonProject/main.py
@@ -5,34 +5,12 @@ import itertools
 input = sys.stdin.readline
 
 n = int(input().strip())
-
-total_people = []
-answer = []
-
-def solution(answer):
-
-    m = int(input())
-    cnt = 0
-    people = []
-
-    for i in range(m):
-        people.append(tuple(map(int, input().split())))
-
-    people.sort(key = lambda x: x[0])
-
-    # 결국 솔팅 해놨기 때문에, 0번째 인덱스는 무조건 작고! 1번째 인덱스만 비교하면 되는 것.
-    maxValue = people[0][1]
-
-    for i in range(0, m):
-
-        if maxValue > people[i][1]:
-            maxValue = people[i][1]
-            cnt += 1
-
-    answer.append(cnt + 1)
+arr = []
 
 for i in range(n):
-    solution(answer)
+    arr.append(input().strip())
 
-for i in range(n):
-    print(answer[i])
+arr.sort()
+arr.sort(reverse=True ,key = lambda x: (len(x)))
+
+print(arr)
