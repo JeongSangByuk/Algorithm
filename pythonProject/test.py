15cbcc66177c2791f02b7910bc23103a49fe48dd
jsb100800@naver.com <jsb100800@naver.com>
2022-03-02 01:03:17 +0900

    이분그래프

--- pythonProject/test.py
+++ pythonProject/test.py
@@ -5,7 +5,28 @@ import itertools
 
 input = sys.stdin.readline
 
-a= [1,2]
-b, c = a
-print(b)
-print(c)
+dic = {}
+
+dic[0] = [1,2,3]
+dic[1] = [4]
+dic[2] = [5]
+dic[3] = []
+dic[4] = []
+dic[5] = []
+
+def dfs(n):
+    print(n)
+
+    for i in dic[n]:
+
+        if i == 4:
+            return "NO"
+
+        ans = dfs(i)
+
+        if ans == "NO":
+            return "NO"
+
+    return "YES"
+
+dfs(0)
