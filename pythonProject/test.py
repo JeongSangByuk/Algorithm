27b2692dbe517e505d56451d88af09958e6eb784
JeongSangByuk <jsb100800@naver.com>
2022-03-02 18:21:28 +0900

    트리의 지름

--- pythonProject/test.py
+++ pythonProject/test.py
@@ -5,28 +5,9 @@ import itertools
 
 input = sys.stdin.readline
 
-dic = {}
+s = set()
 
-dic[0] = [1,2,3]
-dic[1] = [4]
-dic[2] = [5]
-dic[3] = []
-dic[4] = []
-dic[5] = []
+s.add([1,2])
+s.add([2,1])
 
-def dfs(n):
-    print(n)
-
-    for i in dic[n]:
-
-        if i == 4:
-            return "NO"
-
-        ans = dfs(i)
-
-        if ans == "NO":
-            return "NO"
-
-    return "YES"
-
-dfs(0)
+print(s)
