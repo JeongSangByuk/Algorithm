f7a03ea29ac86d89abe681ac0272f191e18ce717
jsb100800@naver.com <jsb100800@naver.com>
2022-03-11 00:15:30 +0900

    행렬

--- Greedy/BJ 1080 행렬.py
+++ Greedy/BJ 1080 행렬.py
@@ -3,6 +3,14 @@ from collections import deque
 import itertools
 import heapq
 
+# 왜 최적해인가?
+# https://www.acmicpc.net/board/view/13509
+# 문제의 목적은 행렬 A를 행렬 B로 바꾸는 것. 결국 A와 B에서 다른 부분은 반드시 한번 flip해줘야 한다.
+# 여기서 최적해가 되려면 반드시 한 번만 뒤집는 것이 옳다! (3,5번 flip하면 결과는 똑같아지기 때문)
+# 때문에 최적해를 구하기 위해서는 바꿀 필요가 있는 부분은 flip하고,
+# 바꿀 필요 없는 부분은 확정하고 넘어가면 된다.
+# 이것이 그리디하게 최소로 flip하는 방법.
+
 sys.setrecursionlimit(10**7)
 input = sys.stdin.readline
 
