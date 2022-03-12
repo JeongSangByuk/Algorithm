efbd448784a588fefa5beac9be7020ec11cc54a3
jsb100800@naver.com <jsb100800@naver.com>
2022-03-13 04:33:39 +0900

    팰린드롬 만들기

--- 1_test_code/main.py
+++ 1_test_code/main.py
@@ -6,32 +6,58 @@ import heapq
 sys.setrecursionlimit(10**7)
 input = sys.stdin.readline
 
-n = int(input())
-p = int(input())
-g = list(int(input()) for _ in range(p))
-parent = [i for i in range(n + 1)]
+s = list(input().strip())
 
-def find(cur):
-    if parent[cur] == cur:
-        return cur
+s.sort()
 
-    parent[cur] = find(parent[cur])
-    return parent[cur]
+dic = {}
 
-def union(a, b):
-    a = find(a)
-    b = find(b)
-    parent[b] = a
+for i in range(len(s)):
+    if s[i] not in dic:
+        dic[s[i]] = 1
+    else:
+        dic[s[i]] += 1
 
-answer = 0
+l = []
 
-print(parent)
+for i in dic.items():
+    l.append([i[0],i[1]])
 
-for i in g:
-    tmp = find(i)
-    if tmp == 0:
-        break
-    answer += 1
-    union(tmp - 1, tmp)
 
-print(answer)
+def solution():
+    ans = ""
+
+    if len(s) % 2 == 0:
+
+        for i in range(len(l)):
+
+            # 홀수인게 하나라도 있으면 못만든다!
+            if l[i][1] % 2 != 0:
+                return "I'm Sorry Hansoo"
+
+        for i in range(len(l)):
+            ans += (l[i][0] * (l[i][1]//2))
+
+        return ans + ans[::-1]
+
+    else:
+        cnt = 0
+        odd = 0
+        for i in range(len(l)):
+
+            # 홀수인게 하나 이상 있으면 못만든다.
+            if l[i][1] % 2 != 0:
+                cnt += 1
+                odd = i
+
+            if cnt > 1:
+                return "I'm Sorry Hansoo"
+
+
+        l[odd][1] -= 1
+        for i in range(len(l)):
+            ans += (l[i][0] * (l[i][1]//2))
+
+        return ans + l[odd][0] + ans[::-1]
+
+print(solution())
