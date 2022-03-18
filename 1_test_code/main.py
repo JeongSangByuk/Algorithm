2058ed2185fa5332c86e2f60d98372646eb36117
jsb100800@naver.com <jsb100800@naver.com>
2022-03-19 02:55:34 +0900

    01타일

--- 1_test_code/main.py
+++ 1_test_code/main.py
@@ -3,18 +3,17 @@ from collections import deque
 import itertools
 import heapq
 
-# https://jyeonnyang2.tistory.com/56
-# DP의 접근은 문제의 작은 부분부터!
-
 sys.setrecursionlimit(10**7)
 input = sys.stdin.readline
 
 n = int(input())
-g = list(map(int, input().split()))
+dp = [0] * n
+dp[0] = 1
 
-dp = [0]*(n + 1)
-for i in range(1, n+1):
-    for j in range(1, i+1):
-        dp[i] = max(dp[i],g[j - 1] + dp[i - j])
+if n > 1:
+    dp[1] = 2
 
-print(dp[n])
+    for i in range(2,n):
+        dp[i] = (dp[i-2] + dp[i - 1]) % 15746
+
+print((dp[n-1]) % 15746)
