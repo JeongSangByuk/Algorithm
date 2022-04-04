bb0d7843533949f7076183844a1d59b2e293e5a6
jsb100800@naver.com <jsb100800@naver.com>
2022-04-04 21:53:02 +0900

    영화감독 숌

--- 1_test_code/main.py
+++ 1_test_code/main.py
@@ -3,43 +3,17 @@ from collections import deque
 import itertools
 import heapq
 
-n, m = map(int, input().split())
-g = [list(input().strip()) for _ in range(n)]
+s = '666'
 
-ans1 = ['B', 'W', 'B', 'W', 'B', 'W', 'B', 'W']
-ans2 = ['W', 'B', 'W', 'B', 'W', 'B', 'W', 'B']
+n = int(input())
 
+i = 666
+cnt = 0
+while cnt != n:
+    if '666' in str(i):
+        cnt += 1
+    i += 1
 
-def change(tg):
-    cnt = 0
-    cnt2 = 0
-    # print(tg)
-    for i in range(8):
-        for j in range(8):
+print(i - 1)
 
-            if i % 2 == 0:
-                if tg[i][j] != ans1[j]:
-                    cnt += 1
-            else:
-                if tg[i][j] != ans2[j]:
-                    cnt += 1
-
-    for i in range(8):
-        for j in range(8):
-
-            if i % 2 == 0:
-                if tg[i][j] != ans2[j]:
-                    cnt2 += 1
-            else:
-                if tg[i][j] != ans1[j]:
-                    cnt2 += 1
-
-    return min(cnt,cnt2)
-
-ans = 100
-for i in range(n-7):
-    for j in range(m - 7):
-        ans = min(ans,change([g[item][j:j+8] for item in range(i,i+8)]))
-
-print(ans)
 
--- 1_test_code/test.py
+++ 1_test_code/test.py
@@ -3,6 +3,6 @@ from collections import deque
 import itertools
 import heapq
 
-a = [[1,2,3,4],[5,6,7,8],[0,0,0,0]]
+str = '123'
 
-print([a[item][1:3] for item in range(1,3)])
+print('121' in str)
