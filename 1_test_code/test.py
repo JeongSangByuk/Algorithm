7593b8a6f7976c53eeeb0c9b9a82e9a9f88778b6
JeongSangByuk <jsb100800@naver.com>
2022-04-04 17:29:53 +0900

    체스판 다시 칠하기

--- 1_test_code/test.py
+++ 1_test_code/test.py
@@ -3,6 +3,6 @@ from collections import deque
 import itertools
 import heapq
 
-print(7//-3)
-print(-7//3)
-print(-7//-3)
+a = [[1,2,3,4],[5,6,7,8],[0,0,0,0]]
+
+print([a[item][1:3] for item in range(1,3)])
