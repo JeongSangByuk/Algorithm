8f9be43dc0c3ae5fad192f418a3c0e3017307b2c
JeongSangByuk <jsb100800@naver.com>
2022-09-08 22:29:04 +0900

    테트로미노

--- 1_test_code/test.py
+++ 1_test_code/test.py
@@ -1,15 +1,61 @@
+import copy
 import sys
 from collections import deque
 import itertools
 import heapq
 
+sys.setrecursionlimit(10**7)
+input = sys.stdin.readline
 
-d = deque()
+n, m  = map(int, input().split())
 
-d.append(1)
-d.append(2)
-d.append(3)
+g = [list(map(int, input().split())) for _ in range(n)]
 
-print(list(itertools.islice(d,0,2)))
+# dfs 활용
+# https://cijbest.tistory.com/87
 
-print(d)
+maxValue = 0
+
+dy, dx = [-1,1,0,0], [0,0,-1,1]
+
+visited = [[-1] * m for _ in range(n)]
+
+maxValueInG = max(map(max, g))
+
+# ㅗ, ㅜ, ㅓ, ㅏ 제외한 모양들 최대값 계산
+def dfs(y,x,dSum, cnt):
+
+    global maxValue
+
+    # 가지 치기
+    if maxValue >= dSum + maxValueInG * (4-cnt):
+        return
+
+    if cnt == 4:
+        maxValue = max(maxValue,dSum)
+        return
+
+    for i in range(4):
+        ny = y + dy[i]
+        nx = x + dx[i]
+
+        if 0 <= ny < n and 0 <= nx < m and visited[ny][nx] == -1:
+
+            if cnt == 2:
+                visited[ny][nx] = 0
+                dfs(y, x, dSum + g[ny][nx], cnt + 1)
+                visited[ny][nx] = -1
+
+            visited[ny][nx] = 0
+            dfs(ny,nx,dSum + g[ny][nx], cnt + 1)
+            visited[ny][nx] = -1
+
+for i in range(n):
+    for j in range(m):
+        visited[i][j] = 0
+        dfs(i,j,g[i][j],1)
+        visited[i][j] = -1
+
+print(maxValue)
+
+
--- 1_test_code/test2.py
+++ 1_test_code/test2.py
@@ -1,68 +1,42 @@
-import sys
-from collections import deque
-import itertools
+import sys; input = sys.stdin.readline
 
-sys.setrecursionlimit(10**7)
-input = sys.stdin.readline
-
-n, m = map(int, input().split())
-
-dic = {}
-v = {}
-
-for i in range(1, n):
-
-    t1, t2, w = map(int, input().split())
-
-    if t1 in dic:
-        dic[t1].append((t2,w))
+def dfs(r, c, idx, total):
+    global ans
+    if ans >= total + max_val * (3 - idx):
+        return
+    if idx == 3:
+        ans = max(ans, total)
+        print("----")
+        print(visit,r,c)
+        print("----")
+        return
     else:
-        dic[t1] = [(t2,w)]
+        for i in range(4):
+            nr = r + dr[i]
+            nc = c + dc[i]
+            if 0 <= nr < N and 0 <= nc < M and visit[nr][nc] == 0:
+                print(visit, r, c)
+                if idx == 1:
+                    visit[nr][nc] = 1
+                    dfs(r, c, idx + 1, total + arr[nr][nc])
+                    visit[nr][nc] = 0
+                visit[nr][nc] = 1
+                dfs(nr, nc, idx + 1, total + arr[nr][nc])
+                visit[nr][nc] = 0
 
-    if t2 in dic:
-        dic[t2].append((t1,w))
-    else:
-        dic[t2] = [(t1,w)]
 
-for i in range(1, n):
+N, M = map(int, input().split())
+arr = [list(map(int, input().split())) for _ in range(N)]
+visit = [([0] * M) for _ in range(N)]
+dr = [-1, 0, 1, 0]
+dc = [0, 1, 0, -1]
+ans = 0
+max_val = max(map(max, arr))
 
-    t1, t2 = map(int, input().split())
+for r in range(N):
+    for c in range(M):
+        visit[r][c] = 1
+        dfs(r, c, 0, arr[r][c])
+        visit[r][c] = 0
 
-    v[t1] = t2
-
-answer_visited = set()
-visited = set()
-
-# print(dic)
-# print(v)
-
-answer = [(0,0)]
-
-def dfs(now, nowPass):
-
-    global answer
-
-    visited.add(now)
-
-    for i in dic[now]:
-
-        if not i[0] in visited:
-
-            tmpMoney = 0
-            nowPass += i[0]
-
-            if i[0] in v:
-                tmpMoney = v[i[0]] - (nowPass * 2)
-
-            if not i[0] in answer_visited and answer[0][1] < tmpMoney:
-                answer.clear()
-                answer.append((i[0], tmpMoney))
-            elif not i[0] in answer_visited and answer[0][1] == tmpMoney:
-                answer.append((i[0], tmpMoney))
-
-            dfs(i[0], nowPass)
-
-dfs(1, 0)
-
-answer.sort(key= lambda x : x[1])
-print(answer[0][1],answer[0][0])
+print(ans)
