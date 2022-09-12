e6e9bf3c3f1478a3db4ebb662449472f419d281b
JeongSangByuk <jsb100800@naver.com>
2022-09-12 17:48:17 +0900

    주차 요금 계산

--- 1_test_code/test2.py
+++ 1_test_code/test2.py
@@ -1,42 +1,20 @@
-import sys; input = sys.stdin.readline
-
-def dfs(r, c, idx, total):
-    global ans
-    if ans >= total + max_val * (3 - idx):
-        return
-    if idx == 3:
-        ans = max(ans, total)
-        print("----")
-        print(visit,r,c)
-        print("----")
-        return
-    else:
-        for i in range(4):
-            nr = r + dr[i]
-            nc = c + dc[i]
-            if 0 <= nr < N and 0 <= nc < M and visit[nr][nc] == 0:
-                print(visit, r, c)
-                if idx == 1:
-                    visit[nr][nc] = 1
-                    dfs(r, c, idx + 1, total + arr[nr][nc])
-                    visit[nr][nc] = 0
-                visit[nr][nc] = 1
-                dfs(nr, nc, idx + 1, total + arr[nr][nc])
-                visit[nr][nc] = 0
+[180, 5000, 10, 600]
+["05:34 5961 IN", "06:00 0000 IN", "06:34 0000 OUT", "07:59 5961 OUT", "07:59 0148 IN", "18:59 0000 IN", "19:09 0148 OUT", "22:59 5961 IN", "23:00 5961 OUT"]
 
 
-N, M = map(int, input().split())
-arr = [list(map(int, input().split())) for _ in range(N)]
-visit = [([0] * M) for _ in range(N)]
-dr = [-1, 0, 1, 0]
-dc = [0, 1, 0, -1]
-ans = 0
-max_val = max(map(max, arr))
 
-for r in range(N):
-    for c in range(M):
-        visit[r][c] = 1
-        dfs(r, c, 0, arr[r][c])
-        visit[r][c] = 0
 
-print(ans)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- Implementation/PG k진수에서 소수 개수 구하기.py
+++ Implementation/PG k진수에서 소수 개수 구하기.py
@@ -8,58 +8,10 @@ import heapq
 sys.setrecursionlimit(10**7)
 input = sys.stdin.readline
 
-def make(num,base):
 
-    tmp = ""
-    while num:
-        tmp = str(num % base) + tmp
-        num = num//base
-    return tmp
-
-# 소수 판별
-def is_prime_number(x):
-
-    if x == 1 or x == 0:
-        return False
-
-    for i in range (2, int(math.sqrt(x) + 1)):
-    	if x % i == 0:
-        	return False
-    return True
-
-def solution(n, k):
-    answer = 0
-
-    primeN = str(make(n,k))
-
-    l = len(primeN)
-
-    #print(primeN)
-
-    i = 0
-    start = 0
-
-    while i < l:
-
-        if primeN[i] != str(0) and i != l -1:
-            i += 1
-            continue
-
-
-        if i == l - 1:
-            p = primeN[start:l]
-        else:
-            p = primeN[start:i]
-
-        if is_prime_number(int(p)):
-            #print(int(p))
-            answer += 1
-        start = i
-        i += 1
-
+def solution(fees, records):
+    answer = []
     return answer
-
-print(solution(110011,10))
 
 
 
