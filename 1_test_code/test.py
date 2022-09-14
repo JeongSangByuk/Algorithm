9c16fbc57c1c0be5718eeb7516dd650907686ab9
JeongSangByuk <jsb100800@naver.com>
2022-09-15 04:12:47 +0900

    양과늑대

--- 1_test_code/test.py
+++ 1_test_code/test.py
@@ -7,55 +7,17 @@ import heapq
 sys.setrecursionlimit(10**7)
 input = sys.stdin.readline
 
-n, m  = map(int, input().split())
+a = set()
 
-g = [list(map(int, input().split())) for _ in range(n)]
+a.add(2)
 
-# dfs 활용
-# https://cijbest.tistory.com/87
+b = a.copy()
 
-maxValue = 0
+b.add(3)
 
-dy, dx = [-1,1,0,0], [0,0,-1,1]
+b.remove(2)
 
-visited = [[-1] * m for _ in range(n)]
-
-maxValueInG = max(map(max, g))
-
-# ㅗ, ㅜ, ㅓ, ㅏ 제외한 모양들 최대값 계산
-def dfs(y,x,dSum, cnt):
-
-    global maxValue
-
-    # 가지 치기
-    if maxValue >= dSum + maxValueInG * (4-cnt):
-        return
-
-    if cnt == 4:
-        maxValue = max(maxValue,dSum)
-        return
-
-    for i in range(4):
-        ny = y + dy[i]
-        nx = x + dx[i]
-
-        if 0 <= ny < n and 0 <= nx < m and visited[ny][nx] == -1:
-
-            if cnt == 2:
-                visited[ny][nx] = 0
-                dfs(y, x, dSum + g[ny][nx], cnt + 1)
-                visited[ny][nx] = -1
-
-            visited[ny][nx] = 0
-            dfs(ny,nx,dSum + g[ny][nx], cnt + 1)
-            visited[ny][nx] = -1
-
-for i in range(n):
-    for j in range(m):
-        visited[i][j] = 0
-        dfs(i,j,g[i][j],1)
-        visited[i][j] = -1
-
-print(maxValue)
+print(a)
+print(b)
 
 
