33d4e0dc3349b44158b63ac27e3a4c955f077fa5
JeongSangByuk <jsb100800@naver.com>
2022-09-21 01:48:01 +0900

    등산코스 정하기

--- 1_test_code/test2.py
+++ 1_test_code/test2.py
@@ -1,6 +1,32 @@
+import heapq
+import sys
 
-["AN", "CF", "MJ", "RT", "NA"]
-[5, 3, 2, 7, 5]
+def solution(n, paths, gates, summits):
 
-["TR", "RT", "TR"]
-[7, 1, 3]
+    def get_min_intensity():
+        pq = []  # (intensity, 현재 위치)
+        visited = [10000001] * (n + 1)
+
+        # 모든 출발지를 우선순위 큐에 삽입
+        for gate in gates:
+            heapq.heappush(pq, (gate,0))
+
+            # 출발지 0으로 초기화
+            visited[gate] = 0
+
+        while pq:
+
+            node, intensity = heapq.heappop(pq)
+
+            if node in summits or intensity > visited[node]:
+                continue
+
+            for next_node,weight in graph[node]:
+
+                ni = max(intensity,weight)
+
+                if ni < visited[next_node]:
+                    visited[next_node] = ni
+                    heapq.heappush(pq, (new_intensity, next_node))
+
+
