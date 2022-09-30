7cfccee9217d4100ea1927ef801d2795b4fb97d2
JeongSangByuk <jsb100800@naver.com>
2022-09-30 13:23:50 +0900

    숨바꼭질3

--- 1_test_code/test.py
+++ 1_test_code/test.py
@@ -8,23 +8,12 @@ import heapq
 sys.setrecursionlimit(10**7)
 input = sys.stdin.readline
 
-<<<<<<< HEAD
-d = [2,3]
 
-d2 = [4,5]
 
-l = d + d2
+for i in range((i + 1, i - 1, 2 * i)):
+    print(i)
 
-heapq.heapify(l)
 
-print(l)
-=======
-v = set()
-q = deque()
-q.append(1)
 
-v.add(tuple(q))
->>>>>>> f46e9e51e88c17e2adc0cc6461436951a6d56390
 
-print(v)
 
