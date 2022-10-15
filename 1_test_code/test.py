fca6b6b77da7a54a5eb593b3a5dea16ccab8a192
JeongSangByuk <jsb100800@naver.com>
2022-10-16 00:47:55 +0900

    행복한 유치원

--- 1_test_code/test.py
+++ 1_test_code/test.py
@@ -10,13 +10,7 @@ input = sys.stdin.readline
 
 a = [-10, -10, 2, 3, 3, 6, 7, 10, 10, 10]
 
-dic = defaultdict(int)
-for i in a:
-    dic[i] += 1
 
-print(dic)
-
-a = list(set(a))
-a.sort()
+a.remove(10)
 
 print(a)
