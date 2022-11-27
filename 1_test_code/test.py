2f22759235b838ffd4437ef55afe0200640f42a1
JeongSangByuk <jsb100800@naver.com>
2022-11-27 14:48:57 +0900

    설탕 배당

--- 1_test_code/test.py
+++ 1_test_code/test.py
@@ -9,16 +9,22 @@ import heapq
 input = sys.stdin.readline
 sys.setrecursionlimit(10 ** 6)
 
-dic = dict()
+n = int(input())
 
-dic[((1,2),(2,2,))] = "t"
+g = [int(input()) for _ in range(n)] + [0] * (10000 - n)
 
-print(dic.keys())
+dp = [0] * 10000
+dp[0] = g[0]
+dp[1] = g[0] + g[1]
+dp[2] = max(g[0] + g[2], g[1] + g[2], dp[1])
 
+for i in range(3, n):
 
+    a = dp[i - 2] + g[i]
+    b = dp[i - 3] + g[i - 1] + g[i]
+    c = dp[i - 1]
 
+    dp[i] = max(a,b,c)
 
-
-
-
+print(max(dp))
 
