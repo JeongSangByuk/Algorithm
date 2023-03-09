16b9430d9e1ffde1568418f275a83ead75e5fc2c
JeongSangByuk <jsb100800@naver.com>
2023-03-09 21:45:14 +0900

    다익스트라 base 수정

--- 1_test_code/main.py
+++ 1_test_code/main.py
@@ -1,6 +1,7 @@
 import math
 import sys
 from collections import deque
+from collections import defaultdict
 import itertools
 import heapq
 from bisect import bisect_left
@@ -8,29 +9,63 @@ from bisect import bisect_left
 # sys.setrecursionlimit(10 ** 7)
 input = sys.stdin.readline
 
-t = int(input())
 
-def sol():
+n = int(input())
+m = int(input())
 
-    n = int(input())
-    g = list(input().strip() for _ in range(n))
-    g.sort()
+dic = defaultdict(list)
 
-    g = list(map(str, g))
-    print(g)
+for i in range(m):
+    a,b,c = map(int, input().split())
+    dic[a].append((b,c))
 
-    for i in range(n - 1):
-        l = len(g[i])
-        if g[i] == g[i + 1][:l]:
-            return False
+start, end = map(int, input().split())
 
-    return True
+def bfs(start, end):
 
-for _ in range(t):
-    if sol():
-        print("YES")
-    else:
-        print("NO")
+    que = []
+    heapq.heappush(que, (0, start))
+
+    v = [9e9 for _ in range(n + 1)]
+    v[start] = 0
+
+    while que:
+
+        d, now = heapq.heappop(que)
+
+        if v[now] < d:
+            continue
+
+        for i in dic[now]:
+
+            _next, _d = i
+            cost = d + _d
+
+            if cost < v[_next]:
+                v[_next] = cost
+                heapq.heappush(que, (cost, _next))
+
+    return v[end]
+
+print(bfs(start,end))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
