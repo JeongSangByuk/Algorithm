a4bcdd44bae7ffec5dc05548bb6448b9ca13f5a9
JeongSangByuk <jsb100800@naver.com>
2023-03-25 21:40:14 +0900

    게리멘더링

--- DFS, BFS/BJ 17471 게리맨더링.py
+++ DFS, BFS/BJ 17471 게리맨더링.py
@@ -1,91 +1,65 @@
-import copy
-import sys
 import math
+import sys
 from collections import deque
 from collections import defaultdict
 import itertools
 import heapq
+from bisect import bisect_left
 
 input = sys.stdin.readline
-
-sys.setrecursionlimit(10 ** 6)
+sys.setrecursionlimit(10 ** 7)
 
 n = int(input())
-people = list(map(int, input().split()))
-p = [list(map(int, input().split())) for _ in range(n)]
-
-dic = dict()
-
-for i in range(1,n+1):
-    dic[i] = []
+g = list(map(int, input().split()))
+dic = defaultdict(set)
 
 for i in range(n):
+    t = list(map(int, input().split()))
 
-    for j in p[i][1:]:
+    for j in range(1, len(t)):
+        dic[i + 1].add(t[j])
 
-        if (i + 1) not in dic[j]:
-            dic[j].append(i+1)
-        if j not in dic[i + 1]:
-            dic[i+1].append(j)
+ar = set()
 
-def count(s):
-    tmp = 0
-    for i in s:
-        tmp += people[i-1]
-    return tmp
+for k in range(1, n // 2 + 1):
+    cnt = 0
+    it = list(itertools.combinations([i for i in range(1, n + 1)], k))
 
-def is_connect(a, b):
+    for i in it:
+        base = set(j for j in range(1, n + 1)).difference(set(i))
+        t = tuple((i, tuple(base)))
+        ar.add(t)
 
-    visited = set()
+
+def bfs(tg):
+    start = tg[0]
+
     que = deque()
+    visit = set()
+    tg = set(tg)
 
-    t = a[0]
-
-    que.append(t)
-    visited.add(t)
-    result = 0
+    que.append(start)
+    visit.add(start)
 
     while que:
 
-        result += 1
         node = que.popleft()
 
         for i in dic[node]:
-
-            if i not in visited and i not in b and i in a:
+            if i not in visit and i in tg:
                 que.append(i)
-                visited.add(i)
+                visit.add(i)
 
-    if result == len(a):
-        return True
-
-    return False
+    return len(tg) == len(visit)
 
 
+result = 9e9
 
-base = {i for i in range(1, n + 1)}
-ans = []
+for i in ar:
+    if bfs(i[0]) and bfs(i[1]):
+        a = sum(list(g[j - 1] for j in i[0]))
+        b = sum(list(g[j - 1] for j in i[1]))
 
-for i in range(1, n//2 + 1):
+        result = min(result, abs(a - b))
 
-    l = list(itertools.combinations(base, i))
-
-    for a in l:
-        a = set(a)
-        b = base - a
-
-        #print(a,b)
-
-        if is_connect(list(a), list(b)) and is_connect(list(b),list(a)):
-            ans.append(abs(count(a) - count(b)))
-
-
-if ans:
-    print(min(ans))
-else:
-    print(-1)
-
-
-
-
-
+print(result if result != 9e9 else -1)
