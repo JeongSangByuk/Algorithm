dfa763515ff252de6bbd59e779121852ca9f580c
JeongSangByuk <jsb100800@naver.com>
2023-04-14 03:17:19 +0900

    LIS 수정

--- Binary Search/BJ 12015 가장 긴 증가하는 부분 수열2.py
+++ Binary Search/BJ 12015 가장 긴 증가하는 부분 수열2.py
@@ -1,40 +1,30 @@
+import math
 import sys
 from collections import deque
+from collections import defaultdict
 import itertools
 import heapq
-from bisect import bisect_left
+from bisect import bisect_left, bisect_right
 
-sys.setrecursionlimit(10**7)
 input = sys.stdin.readline
 
 n = int(input())
 g = list(map(int, input().split()))
-dp = [0]
+dp = [g[0]]
 
 for i in g:
 
     if dp[-1] < i:
         dp.append(i)
+        # print(dp)
 
-    else:
-        # left = 0
-        # right = len(dp)
-        #
-        # # 이분탐색으로 들어갈 인덱스를 찾는다.
-        # # 결국 길이를 구하는 것이기 때문에, dp 배열 자체에 값은 중요하지 않다.
-        # while left < right:
-        #     mid = (left + right) // 2
-        #
-        #     if dp[mid] < i:
-        #         left = mid + 1
-        #     else:
-        #         right = mid
-        #
-        # dp[right] = i
+        continue
 
-        dp[bisect_left(dp, i)] = i
+    lo = bisect_left(dp, i)
 
+    dp[lo] = i
 
+    # print(dp)
 
-#print(dp)
-print(len(dp) - 1)
+print(len(dp))
+
