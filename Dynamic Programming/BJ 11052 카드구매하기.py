49ba391b69b5339aca026f9bc80202c65123f370
JeongSangByuk <jsb100800@naver.com>
2023-04-14 17:17:56 +0900

    카드 구매하기

--- Dynamic Programming/BJ 11052 카드구매하기.py
+++ Dynamic Programming/BJ 11052 카드구매하기.py
@@ -1,20 +1,29 @@
+import math
 import sys
 from collections import deque
+from collections import defaultdict
 import itertools
 import heapq
+from bisect import bisect_left
 
-# https://jyeonnyang2.tistory.com/56
-# DP의 접근은 문제의 작은 부분부터!
-
-sys.setrecursionlimit(10**7)
 input = sys.stdin.readline
 
 n = int(input())
-g = list(map(int, input().split()))
+g = [0] + list(map(int, input().split()))
 
-dp = [0]*(n + 1)
-for i in range(1, n+1):
-    for j in range(1, i+1):
-        dp[i] = max(dp[i],g[j - 1] + dp[i - j])
+dp = [0] * (n + 1)
+
+for i in range(1, n + 1):
+    dp[i] = g[i]
+
+for i in range(2, n + 1):
+
+    tmp = list()
+
+    t = i // 2
+
+    for j in range(1, t + 1):
+        dp[i] = max(dp[i], dp[j] + dp[i - j])
 
 print(dp[n])
+
